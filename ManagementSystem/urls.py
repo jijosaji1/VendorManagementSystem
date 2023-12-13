fb2e097a74e87f6bff619a1ce54a0b0f8b973ae3
jijosaji <jsaji9354@gmail.com>
2023-12-13 22:04:08 +0530

    Updated files

--- ManagementSystem/urls.py
+++ ManagementSystem/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for VendorManagementSystem project.
+URL configuration for ManagementSystem project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
